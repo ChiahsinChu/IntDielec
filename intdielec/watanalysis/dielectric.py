605e7069be82cf994b12e351f5336690aa3d8f13
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2023-04-18 13:39:19 +0200

    add var to choose if calculating unscaled data in AdInverseDielectricConstant

--- intdielec/watanalysis/dielectric.py
+++ intdielec/watanalysis/dielectric.py
@@ -40,10 +40,6 @@ class InverseDielectricConstant(AnalysisBase):
                                       " systems with free charges are not"
                                       " available.")
 
-        # self.results.m_lo = np.zeros((self.nbins))
-        # self.results.mM_lo = np.zeros((self.nbins))
-        # self.results.m_hi = np.zeros((self.nbins))
-        # self.results.mM_hi = np.zeros((self.nbins))
         self.results.m = np.zeros((self.nbins))
         self.results.mM = np.zeros((self.nbins))
         self.results.M = 0.
@@ -125,10 +121,12 @@ class AdInverseDielectricConstant(InverseDielectricConstant):
                  surf_ids,
                  cutoff=2.7,
                  sfactor=2,
+                 calc_unscaled=False,
                  **kwargs) -> None:
         super().__init__(atomgroups, bin_edges, surf_ids, **kwargs)
         self.cutoff = cutoff
         self.sfactor = sfactor
+        self.calc_unscaled = calc_unscaled
 
     def _prepare(self):
         super()._prepare()
@@ -158,26 +156,27 @@ class AdInverseDielectricConstant(InverseDielectricConstant):
         ts_volume = ts_area * (z_hi - z_lo - 2 * self.img_plane)
         self.results.volume += ts_volume
 
-        # unscaled charges
-        # charge density [e/A^3]
-        rho, bin_edges = np.histogram(self.atoms.positions[:, 2],
-                                      bins=bin_edges,
-                                      weights=self.atoms.charges)
-        bin_volumes = np.diff(bin_edges) * ts_area
-        rho /= bin_volumes
-        _m = -integrate.cumulative_trapezoid(rho, bins, initial=0)
-        # M
-        M = np.sum(_m * bin_volumes)
-        self.results.M += M
-        self.results.M2 += (M**2)
-        # lo surf m
-        m = np.interp(self.bins + z_lo, bins, _m)
-        self.results.m += m
-        self.results.mM += (m * M)
-        # hi surf m
-        m = np.interp(np.sort(z_hi - self.bins), bins, _m)
-        self.results.m += np.flip(m)
-        self.results.mM += np.flip(m * M)
+        if self.calc_unscaled:
+            # unscaled charges
+            # charge density [e/A^3]
+            rho, bin_edges = np.histogram(self.atoms.positions[:, 2],
+                                          bins=bin_edges,
+                                          weights=self.atoms.charges)
+            bin_volumes = np.diff(bin_edges) * ts_area
+            rho /= bin_volumes
+            _m = -integrate.cumulative_trapezoid(rho, bins, initial=0)
+            # M
+            M = np.sum(_m * bin_volumes)
+            self.results.M += M
+            self.results.M2 += (M**2)
+            # lo surf m
+            m = np.interp(self.bins + z_lo, bins, _m)
+            self.results.m += m
+            self.results.mM += (m * M)
+            # hi surf m
+            m = np.interp(np.sort(z_hi - self.bins), bins, _m)
+            self.results.m += np.flip(m)
+            self.results.mM += np.flip(m * M)
 
         # scaled charges
         # scale charges of chemisorbed water
@@ -224,93 +223,3 @@ class AdInverseDielectricConstant(InverseDielectricConstant):
             "eV") * self.temperature * constants["electric_constant"]
         self.results.inveps_scaled = 1 - x_fluct / (
             const + M_fluct / self.results.volume)
-
-
-# class ParallelInverseDielectricConstant(InverseDielectricConstant):
-#     def __init__(self,
-#                  atomgroups,
-#                  bin_edges,
-#                  surf_ids,
-#                  axis: int = 2,
-#                  temperature=330,
-#                  img_plane=0,
-#                  make_whole=False,
-#                  verbose=False) -> None:
-#         super().__init__(atomgroups, bin_edges, surf_ids, axis, temperature,
-#                          img_plane, make_whole, verbose)
-#         #parallel value initial
-#         self.para = None
-#         self._para_region = None
-
-#     def _conclude(self):
-#         pass
-
-#     def _parallel_init(self, *args, **kwargs):
-#         start = self._para_region.start
-#         stop = self._para_region.stop
-#         step = self._para_region.step
-#         self._setup_frames(self._trajectory, start, stop, step)
-#         self._prepare()
-
-#     def _run(self, start=None, stop=None, step=None, verbose=None):
-
-#         #self._trajectory._reopen()
-#         if verbose == True:
-#             print(" ", end='')
-#         super().run(start, stop, step, verbose)
-
-#         if self.para:
-#             block_result = self._para_block_result()
-#             if block_result == None:
-#                 raise ValueError(
-#                     "in parallel, block result has not been defined or no data output!"
-#                 )
-#             #logger.info("block_anal finished.")
-#             return block_result
-
-#     def _para_block_result(self):
-#         return [
-#             self.results.m, self.results.mM, self.results.M, self.results.M2,
-#             self.volume
-#         ]
-
-#     def _parallel_conclude(self, rawdata):
-#         # set attributes for further analysis
-#         method_attr = rawdata[-1]
-#         del rawdata[-1]
-#         self.start = method_attr[0]
-#         self.stop = method_attr[1]
-#         self.step = method_attr[2]
-#         self.frames = np.arange(self.start, self.stop, self.step)
-#         self.n_frames = len(self.frames)
-
-#         self.results["m"] = np.zeros((self.nbins))
-#         self.results["mM"] = np.zeros((self.nbins))
-#         self.results["M"] = 0
-#         self.results["M2"] = 0
-#         self.volume = 0
-
-#         for single_data in rawdata:
-#             self.results["m"] += single_data[0]
-#             self.results["mM"] += single_data[1]
-#             self.results["M"] += single_data[2]
-#             self.results["M2"] += single_data[3]
-#             self.volume += single_data[4]
-
-#         self.results["m"] /= self.n_frames
-#         self.results["mM"] /= self.n_frames
-#         self.results["M"] /= self.n_frames
-#         self.results["M2"] /= self.n_frames
-#         self.volume /= self.n_frames
-
-#         x_fluct = self.results["mM"] - self.results["m"] * self.results["M"]
-#         M_fluct = self.results["M2"] - self.results["M"] * self.results["M"]
-#         const = convert(
-#             constants["Boltzman_constant"], "kJ/mol",
-#             "eV") * self.temperature * constants["electric_constant"]
-#         self.results["inveps"] = 1 - x_fluct / (const + M_fluct / self.volume)
-
-#         return "FINISH PARA CONCLUDE"
-
-#     def run(self, start=None, stop=None, step=None, n_proc=1):
-#         parallel_exec(self._run, start, stop, step, n_proc)
