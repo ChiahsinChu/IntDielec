203bf007f28866938b85b60573dbdf01f85f2805
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2023-06-09 13:54:00 +0800

    add eps_ave and c_d in OrientEps plotting

--- intdielec/workflow/orient_eps.py
+++ intdielec/workflow/orient_eps.py
@@ -3,7 +3,7 @@ from ase import Atoms
 from MDAnalysis import transformations as trans
 
 from ..exts.toolbox.toolbox import plot
-from ..exts.toolbox.toolbox.plot.figure import FullCellFigure, HalfCellFigure
+from ..exts.toolbox.toolbox.plot.figure import FullCellFigure, Figure
 from ..exts.toolbox.toolbox.utils.math import *
 from ..exts.toolbox.toolbox.utils.unit import *
 from ..exts.toolbox.toolbox.utils.utils import safe_makedirs, load_dict
@@ -20,7 +20,7 @@ class OrientEps(Eps):
                  topo: str = None,
                  coord: str = None,
                  data_fmt: str = "pkl",
-                 dimensions=None, 
+                 dimensions=None,
                  **kwargs) -> None:
         super().__init__(work_dir, data_fmt)
 
@@ -65,30 +65,96 @@ class OrientEps(Eps):
         self._save_data()
         self.make_plots()
 
-    def make_plots(self, half=False):
+    def make_plots(self):
         if not hasattr(self, "results"):
-            self.results = load_dict(self.work_dir, "eps_data.%s" % self.data_fmt)
+            self.results = load_dict(self.work_dir,
+                                     "eps_data.%s" % self.data_fmt)
 
         work_dir = os.path.join(self.work_dir, "figures")
         safe_makedirs(work_dir)
 
+        xlim = (0, self.universe._trajectory.ts.dimensions[2])
         # inveps
         figure = FullCellFigure()
-        figure.setup(self.results["bins"], 
-                     self.results["inveps"], 
-                     z_surfs=(self.results["z_lo"], self.results["z_hi"]), 
+        figure.setup(self.results["bins"],
+                     self.results["inveps"],
+                     xlim=xlim,
+                     z_surfs=(self.results["z_lo"], self.results["z_hi"]),
                      color="blue")
         figure.set_labels("orient_inveps")
         figure.fig.savefig(os.path.join(work_dir, "orient_inveps.png"))
 
         # local polarization distribution
         figure = FullCellFigure()
-        figure.setup(self.results["bins"], 
-                     self.results["m"], 
-                     z_surfs=(self.results["z_lo"], self.results["z_hi"]), 
+        figure.setup(self.results["bins"],
+                     self.results["m"],
+                     xlim=xlim,
+                     z_surfs=(self.results["z_lo"], self.results["z_hi"]),
                      color="blue")
         figure.set_labels("polarization")
         figure.fig.savefig(os.path.join(work_dir, "polarization.png"))
+
+        # eps_ave
+        lz = self.universe._trajectory.ts.dimensions[2] / 2 - self.results[
+            "z_lo"]
+        xlim = (0, lz)
+        x = np.linspace(0, lz)
+        xp = self.results["bins"] - self.results["z_lo"]
+        fp = self.results["inveps"]
+        y_lo = np.interp(x, xp, fp)
+        xp = np.sort(self.results["z_hi"] - self.results["bins"])
+        fp = np.flip(self.results["inveps"])
+        y_hi = np.interp(x, xp, fp)
+        inveps = (y_lo + y_hi) / 2
+        ave_eps = 1. / cumave(inveps)
+
+        figure = Figure()
+        ax = figure.ax
+
+        color = "blue"
+        figure.setup(x, inveps, xlim=xlim, color=color)
+        figure.set_labels("orient_inveps")
+        ax.tick_params(axis="y", labelcolor=color)
+        ax.axhline(y=0., color=color, ls="--", alpha=0.5)
+        ax.axhline(y=1., color=color, ls="--", alpha=0.5)
+
+        ax_right = ax.twinx()
+        color = "red"
+        ax_right.plot(x, ave_eps, xlim=xlim, color=color)
+        ax_right.set_ylim(bottom=0.)
+        ax_right.tick_params(axis="y", labelcolor=color)
+        ax_right.axhline(y=1., color=color, ls="--", alpha=0.5)
+        ax_right.axvline(x=6., color=color, ls="--", alpha=0.5)
+        ax_right.set_ylabel(r"$\varepsilon_{ave}$", color=color)
+
+        figure.fig.savefig(os.path.join(work_dir, "eps_ave.png"))
+
+        # differential capacitance
+        const = constants.epsilon_0 / constants.micro * constants.centi / (
+            constants.angstrom / constants.centi)
+
+        figure = Figure()
+        ax = figure.ax
+
+        color = "blue"
+        figure.setup(x, inveps, xlim=xlim, color=color)
+        figure.set_labels("orient_inveps")
+        ax.tick_params(axis="y", labelcolor=color)
+        ax.axhline(y=0., color=color, ls="--", alpha=0.5)
+        ax.axhline(y=1., color=color, ls="--", alpha=0.5)
+
+        ax_right = ax.twinx()
+        color = "red"
+        c_d = ave_eps / x * const
+        ax_right.plot(x, c_d, color=color)
+        ax_right.set_ylim(0., np.min([c_d.max(), 20]))
+        ax_right.tick_params(axis="y", labelcolor=color)
+        ax_right.axhline(y=1., color=color, ls="--", alpha=0.5)
+        ax_right.axvline(x=6., color=color, ls="--", alpha=0.5)
+        ax_right.set_ylabel(r"${\rm C_d}$ [$\mu F/cm^2$]", color=color)
+
+        figure.fig.savefig(
+            os.path.join(work_dir, "differential_capacitance.png"))
 
     def _get_atom_style(self):
         # atom_style="id resid type charge x y z"
@@ -114,4 +180,3 @@ class OrientEps(Eps):
                       pbc=True)
         atoms.set_cell(self.universe._trajectory.ts.dimensions)
         return atoms
-
