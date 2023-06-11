98d0a8b25b94e4cc69aa48ccf00c57ba49b499ec
Jia-Xin Zhu <xmuchiahsin@gmail.com>
2023-06-11 11:36:12 +0800

    add test for OrientEps.make_plots

---
+++ tests/workflow/test_orienteps/run.py
@@ -0,0 +1,11 @@
+from intdielec.workflow.orient_eps import OrientEps
+
+
+eps_calc = OrientEps(work_dir="eps_calc",
+                     topo="eps_calc/interface.psf",
+                     coord="eps_calc/dump.xtc",
+                     dimensions=[11.246, 11.246, 35.94, 90, 90, 90])
+
+eps_calc.make_plots()
+
+
