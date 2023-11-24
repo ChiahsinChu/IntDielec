ed6df18f315ed9beb249268f6b690c50c796d6d0
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2023-11-24 10:23:56 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,4 @@ setup(name="intdielec",
       version="1.0",
       packages=find_packages(),
       include_package_data=True,
-      install_requires=["MDAnalysis == 2.3.0", "numpy == 1.20"])
+     )
